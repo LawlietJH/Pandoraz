c1937ef36e399ccb47e72eddfb9a986de7c16d1f
Lawliet <jerry_howl_lawliet@hotmail.com>
2017-04-06 01:17:30 -0500

    Agregada Información A El Evento Del Teclado 'Ctrl+I'
    Se Agregó Información Al Evento Del Teclado 'Ctrl+I'

--- Pandoraz.py
+++ Pandoraz.py
@@ -675,9 +675,17 @@ def GetInfo():
 		os.system("cls && Title Pandoraz.py                "+\
 				"By: LawlietJH                "+Version+"    ")
 		
-		print("\n\n\n\t xD Aún No Hay Información Por Mostrar.")
+		print("\n\n\n    [+] Información:", end="\n\n\n\n\t")
+		print("  [*] 'Ctrl+C':\t  Si Se Está En Un Escaneo, Se Podra Salir", end="\n\t\t\t")
+		print("  Precionando 'Ctrl+C'.",end="\n\n\t\t\t")
+		print("  Si Hay Más De Una Interfaz De Red Para Elegir,", end="\n\t\t\t")
+		print("  Volverá Al Menú De 'Selección De Interfaz'.", end="\n\t\t\t")
+		print("  Para Salir En Esta Ocasión Del Script,", end="\n\t\t\t")
+		print("  Vuelva A Precionar 'Ctrl+C' En El Menú.")
+		print("\n\n\n\t  [*] 'Ctrl+I':\t  Muestra Esta Información.")
 		
-		#~ os.system("Pause>Nul")
+		
+		os.system("Pause>Nul")
 		time.sleep(1.5)
 		
 	except KeyboardInterrupt:
