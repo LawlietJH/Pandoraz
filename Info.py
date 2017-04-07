f3664a6d28d645a9a58461068516678a7a0c7c63
Lawliet <jerry_howl_lawliet@hotmail.com>
2017-04-07 01:03:25 -0500

    Archivo "Info.py" con Información Del Script (Ctrl+I)
    Scritp De Información Para Ventana Emergente.

---
+++ Info.py
@@ -0,0 +1,16 @@
+import os
+
+os.system("cls && Title Pandoraz.py                "+\
+		"By: LawlietJH                v1.4.3    ")
+
+print("\n\n\n    [+] Información:", end="\n\n\n\n\t")
+print("  [*] 'Ctrl+C':\t  Si Se Está En Un Escaneo, Se Podra Salir", end="\n\t\t\t")
+print("  Precionando 'Ctrl+C'.",end="\n\n\t\t\t")
+print("  Si Hay Más De Una Interfaz De Red Para Elegir,", end="\n\t\t\t")
+print("  Volverá Al Menú De 'Selección De Interfaz'.", end="\n\t\t\t")
+print("  Para Salir En Esta Ocasión Del Script,", end="\n\t\t\t")
+print("  Vuelva A Precionar 'Ctrl+C' En El Menú.")
+print("\n\n\n\t  [*] 'Ctrl+I':\t  Muestra Esta Información.\n\n\n\n\n\n\n\n\t  [+] ", end="")
+
+
+os.system("Pause")
